aa5894549072c5bf8631357f7ed2a4be562a4389
Verjan <jeison.verjan@unisys.com>
2023-12-08 09:06:48 -0500

    setting colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        white: "#FFF",
+        background: {
+          500: "#1B1B35",
+          600: "#0D0D2C",
+          700: "#13131A",
+        },
+        logo: {
+          500: "#3168D8",
+          700: "#7900FF",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
